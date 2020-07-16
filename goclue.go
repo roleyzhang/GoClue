62be76e1ececd6d27f1c8c9751dd40205aa8c85e
roley zhang <roleyzhang@gmail.com>
2020-07-15 23:25:36 -0700

    added login testing

--- goclue.go
+++ goclue.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
-import "bufio"
-import "os"
-import "strings"
+import (
+	"encoding/json"
+	"fmt"
+	"bufio"
+	"strings"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 
-
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+)
 
 func main() {
 	fmt.Printf("%s\n%s\n", "GoClue is a cloud disk console client.",
@@ -21,13 +30,11 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 		}
 
-	runCommand(cmdString)
+		runCommand(cmdString)
 		// err = runCommand(cmdString)
 		// if err != nil {
 		// 	fmt.Fprintln(os.Stderr, err)
 		// }
-
-
 
 		// fmt.Println("Guess my favorite color:")
 		// if _, err := fmt.Scanf("%s", &guessColor); err != nil {
@@ -43,59 +50,151 @@ func main() {
 
 }
 
-type command struct{
-	name string
+type command struct {
+	name  string
 	param string
-	tip string
+	tip   string
 }
 
-var allCommands []command;
+var allCommands []command
 
 func init() {
 	fmt.Println("This will get called on main initialization")
 	// allCommands = make([]command, 0)
 	allCommands = []command{
-		{"q","","Quit"},
-		{"login","","Login to your account of net drive"},
-		{"mkdir","","Create directory"},
-		{"rm","","Delete directory or file, use \"-r\" for delete directory"},
-		{"cd","","change directory"},
-		{"..","","Exit current directory"},
-		{"d","","Download files use \"-r\" for download directory"},
-		{"ls","","list contents of current directory"},
-		{"u","","Upload directory or file, use \"-r\" for upload directory"},
-		{"h","","Print help"},
-		}
+		{"q", "", "Quit"},
+		{"login", "", "Login to your account of net drive"},
+		{"mkdir", "", "Create directory"},
+		{"rm", "", "Delete directory or file, use \"-r\" for delete directory"},
+		{"cd", "", "change directory"},
+		{"..", "", "Exit current directory"},
+		{"d", "", "Download files use \"-r\" for download directory"},
+		{"ls", "", "list contents of current directory"},
+		{"u", "", "Upload directory or file, use \"-r\" for upload directory"},
+		{"h", "", "Print help"},
+	}
 }
 
 func runCommand(commandStr string) {
 	commandStr = strings.TrimSuffix(commandStr, "\n")
 	arrCommandStr := strings.Fields(commandStr)
-	fmt.Printf("arrCommandStr: %d \n", len(arrCommandStr))
-	switch arrCommandStr[0] {
-	case "q":
-		os.Exit(0)
-	case "login":
-		println("this is login")
-	case "mkdir":
-		println("this is mkdir")
-	case "cd":
-		println("this is cd")
-	case "..":
-		println("this is ..")
-	case "rm":
-		println("this is rm")
-	case "d":
-		println("this is download")
-	case "ls":
-		println("this is ls")
-	case "u":
-		println("this is upload")
-	case "h":
-		for _, cmd := range allCommands {
-			fmt.Printf("%6s: %s \n", cmd.name, cmd.tip)
+	// fmt.Printf("arrCommandStr: %d \n", len(arrCommandStr))
+	if len(arrCommandStr) > 0 {
+		switch arrCommandStr[0] {
+		case "q":
+			os.Exit(0)
+		case "login":
+			println("this is login")
+			loginTest()
+		case "mkdir":
+			println("this is mkdir")
+		case "cd":
+			println("this is cd")
+		case "..":
+			println("this is ..")
+		case "rm":
+			println("this is rm")
+		case "d":
+			println("this is download")
+		case "ls":
+			println("this is ls")
+		case "u":
+			println("this is upload")
+		case "h":
+			for _, cmd := range allCommands {
+				fmt.Printf("%6s: %s \n", cmd.name, cmd.tip)
+			}
+		default:
+			println("Please check your input or type \"h\" get help")
 		}
-	default:
-		println("Please check your input or type \"h\" get help")
+
 	}
+}
+//------------
+
+func loginTest() {
+        b, err := ioutil.ReadFile("credentials.json")
+        if err != nil {
+                log.Fatalf("Unable to read client secret file: %v", err)
+        }
+
+        // If modifying these scopes, delete your previously saved token.json.
+        config, err := google.ConfigFromJSON(b, drive.DriveMetadataReadonlyScope)
+        if err != nil {
+                log.Fatalf("Unable to parse client secret file to config: %v", err)
+        }
+        client := getClient(config)
+
+        srv, err := drive.New(client)
+        if err != nil {
+                log.Fatalf("Unable to retrieve Drive client: %v", err)
+        }
+
+        r, err := srv.Files.List().PageSize(50).
+                Fields("nextPageToken, files(id, name)").Do()
+        if err != nil {
+                log.Fatalf("Unable to retrieve files: %v", err)
+        }
+        fmt.Println("Files:")
+        if len(r.Files) == 0 {
+                fmt.Println("No files found.")
+        } else {
+                for _, i := range r.Files {
+                        fmt.Printf("%s (%s)\n", i.Name, i.Id)
+                }
+        }
+}
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        tokFile := "token.json"
+        tok, err := tokenFromFile(tokFile)
+        if err != nil {
+                tok = getTokenFromWeb(config)
+                saveToken(tokFile, tok)
+        }
+        return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+        authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+        fmt.Printf("Go to the following link in your browser then type the "+
+                "authorization code: \n%v\n", authURL)
+
+        var authCode string
+        if _, err := fmt.Scan(&authCode); err != nil {
+                log.Fatalf("Unable to read authorization code %v", err)
+        }
+
+        tok, err := config.Exchange(context.TODO(), authCode)
+        if err != nil {
+                log.Fatalf("Unable to retrieve token from web %v", err)
+        }
+        return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+        f, err := os.Open(file)
+        if err != nil {
+                return nil, err
+        }
+        defer f.Close()
+        tok := &oauth2.Token{}
+        err = json.NewDecoder(f).Decode(tok)
+        return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+        fmt.Printf("Saving credential file to: %s\n", path)
+        f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+        if err != nil {
+                log.Fatalf("Unable to cache oauth token: %v", err)
+        }
+        defer f.Close()
+        json.NewEncoder(f).Encode(token)
 }
